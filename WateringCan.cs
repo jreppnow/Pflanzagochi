883fcc4a596eeece58eb5429cf006a8f7dadf91e
Janosch <janoschre@gmail.com>
2020-04-18 15:21:17 +0200

    added logging and event to pouring

--- WateringCan.cs
+++ WateringCan.cs
@@ -3,6 +3,8 @@ using System;
 
 public class WateringCan : AnimatedSprite
 {
+	[Signal]
+	public delegate void Pouring(Vector2 location);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,13 +14,27 @@ public class WateringCan : AnimatedSprite
 
 	public override void _Process(float delta)
 	{
+		Vector2 oldPosition = Position;
+		bool wasPouring = IsPlaying();
 		Position = GetViewport().GetMousePosition();
 		if (Input.IsMouseButtonPressed((int) ButtonList.Left))
 		{
-			Animation = "pouring";
-			Play();
+			if (oldPosition != Position || !wasPouring)
+			{
+				EmitSignal("Pouring", Position);	
+				if (!wasPouring)
+				{
+					GD.Print("Started pouring at location {}", Position);
+					Animation = "pouring";
+					Play();
+				}
+			}
 		} else
 		{
+			if (wasPouring)
+			{
+				GD.Print("Stopped pouring at location {}", Position);
+			}
 			Animation = "default";
 			Stop();
 		}
