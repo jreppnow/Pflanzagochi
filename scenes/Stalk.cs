c40c06890f30f71a4064a2fb786e3bba179fe4c9
Janosch <janoschre@gmail.com>
2020-04-19 12:06:56 +0200

    added initial stalk script

---
+++ scenes/Stalk.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Stalk : Area2D
+{
+	public Vector2 HitboxExtends
+	{
+		get { return (GetNode<CollisionShape2D>("CollisionShape2D").Shape as RectangleShape2D).Extents;  }
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public Vector2 ComputeNextLocation()
+	{
+		return Position + HitboxExtends.Rotated(Rotation);
+	}
+
+	public Vector2 ComputeLocation(Vector2 attachedLocation)
+	{
+		return attachedLocation + HitboxExtends.Rotated(Rotation);
+	}
+}
